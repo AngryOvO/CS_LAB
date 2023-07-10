53f8317e8e13f89d68d63c6070f2cde830a7c529
AngryOvO <moonlighthouse99@gmail.com>
2023-07-11 00:05:01 +0900

    process_done

--- cslab_linux_example/Process/exercise_process2.c
+++ cslab_linux_example/Process/exercise_process2.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(void)
 {
 	char *argv[3];
-	
-	printf("Before processing\n");
+	pid_t pid;
+	int status;
 
-	argv[0] = "exercise_process.out";
-	argv[1] = "10";
-	argv[3] = NULL;
-
-	if(execv("./exercise_process.out",argv) == -1)
+	switch(pid = fork())
 	{
-		perror("execv");
-		exit(1);
+		case 0:
+			printf("Before processing\n");
+
+			argv[0] = "exercise_process.out";
+			argv[1] = "10";
+			argv[2] = NULL;
+
+			if(execv("exercise_process.out",argv) == -1)
+			{
+				perror("execv");
+				exit(1);
+			}
+			break;
+		default:
+			wait(&status);
+			printf("After processing\n");
+			break;
 	}
-	
-	printf("After processing\n");
 
 	return 0;
 }
